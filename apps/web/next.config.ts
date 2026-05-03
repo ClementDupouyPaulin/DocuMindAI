bf22006db9462a9505304345d2a6a73df9c3ac4f
ClementDupouyPaulin <clement.dupouy-paulin@etu.unice.fr>
2026-05-03 23:59:56 +0200

    fix: remove github pages config for vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const repoName = "DocuMindAI";
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
